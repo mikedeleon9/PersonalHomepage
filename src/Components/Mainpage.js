db13a1ebb250da8d862968f532d7c3aec7ad5a52
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-19 15:20:56 -0400

    added more styling and the ability to open link in new tab

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -10,11 +10,13 @@ export default function Mainpage(){
     return (
 
 
-        <div className="border-2 border-black flex w-full justify-between">
+        <div className=" flex w-full justify-between">
             {
                 SitesAndLinks.map((site, index) =>(
-                    <div className="" key={index}>
-                        <a href={site.url}>
+                    <div className="flex flex-col items-center" key={index}>
+                        <a href={site.url}
+                          target="_blank" 
+                          rel="noopener noreferrer">
                             <img className="w-16 h-16" src={site.image} alt={site.name} />  
                         </a>
                         <h3>{site.name}</h3>
