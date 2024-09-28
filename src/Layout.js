fb2c75354f804087c44fc43ae7ba83dd78f24785
unknown <mikedeleon9@gmail.com>
2024-09-28 14:07:09 -0400

    adding heading

--- src/Layout.js
+++ src/Layout.js
@@ -4,6 +4,7 @@ import Games from "./components/Games";
 export default function Layout(){
     return(
         <div className="p-4 ">
+            <h1 className="font-Inter text-center text-white text-4xl">My Homepage</h1>
             <Games />
 
         </div>
